3d148340a9a61014c330545d165eaf45268964b2
ergemp <ergemp@gmail.com>
2019-12-31 05:14:32 +0300

    bubble sort added

--- src/main/java/org/ergemp/mlt/sorting/algorithm/BubbleSort.java
+++ src/main/java/org/ergemp/mlt/sorting/algorithm/BubbleSort.java
@@ -1,4 +1,45 @@
 package org.ergemp.mlt.sorting.algorithm;
 
+import java.util.List;
+
 public class BubbleSort {
+    public static List<Integer> sort(List<Integer> gList) {
+        try {
+            Boolean checkSwap = false;
+
+            for (Integer i=0; i<gList.size()-1 ; i++) {
+                if (gList.get(i) > gList.get(i + 1)) {
+
+                    Integer tmp1 ;
+                    Integer tmp2 ;
+                    tmp1 = gList.get(i+1);
+                    tmp2 = gList.get(i);
+
+                    gList.remove(tmp1);
+                    gList.remove(tmp2);
+
+                    gList.add(i, tmp1);
+                    gList.add(i+1, tmp2);
+
+                    checkSwap=true;
+                }
+            }
+
+            //System.out.println(gList.toString());
+            if (checkSwap){
+                sort(gList);
+            }
+            else {
+                return gList;
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            return gList;
+        }
+    }
 }
+
+
--- src/main/java/org/ergemp/mlt/sorting/example/BubbleSortUsage.java
+++ src/main/java/org/ergemp/mlt/sorting/example/BubbleSortUsage.java
@@ -1,4 +1,18 @@
 package org.ergemp.mlt.sorting.example;
 
+import org.ergemp.mlt.sorting.algorithm.BubbleSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BubbleSortUsage {
+    public static void main(String[] args){
+        Integer arr[] = {64,25,12,22,11,5,4,9,10,2,3,7,6};
+        List<Integer> llist = new ArrayList<>();
+
+        for (Integer i=0; i<arr.length; i++){
+            llist.add(arr[i]);
+        }
+        System.out.println(BubbleSort.sort(llist).toString());
+    }
 }
