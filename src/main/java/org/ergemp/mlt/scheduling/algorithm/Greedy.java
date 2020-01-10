7ca8d99de541bf9b2cc281cb822b89c8f7d3a2c3
ergemp <ergemp@gmail.com>
2020-01-10 14:29:45 +0300

    scheduling greedy algoritm added

--- src/main/java/org/ergemp/mlt/scheduling/algorithm/Greedy.java
+++ src/main/java/org/ergemp/mlt/scheduling/algorithm/Greedy.java
@@ -1,4 +1,11 @@
 package org.ergemp.mlt.scheduling.algorithm;
 
+import org.ergemp.mlt.scheduling.model.Jobs;
+
 public class Greedy {
+    public static void run(Jobs gJobs) {
+
+
+
+    }
 }
--- src/main/java/org/ergemp/mlt/scheduling/example/GreedyUsage.java
+++ src/main/java/org/ergemp/mlt/scheduling/example/GreedyUsage.java
@@ -1,4 +1,18 @@
 package org.ergemp.mlt.scheduling.example;
 
+import org.ergemp.mlt.scheduling.model.JobModel;
+import org.ergemp.mlt.scheduling.model.Jobs;
+
 public class GreedyUsage {
+    public static void main(String[] args) {
+        Integer processingArr[] = {64,25,12,22,11,5,4,9,10,2,3,7,6};
+        Jobs myJobs = new Jobs();
+
+        for (Integer i=0; i<processingArr.length; i++){
+            JobModel job = new JobModel();
+            job.setJobName("job" + processingArr[i]);
+            job.setProcessingTime(Long.parseLong(processingArr[i].toString()));
+            myJobs.addJob(job);
+        }
+    }
 }
