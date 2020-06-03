df802529d85c8f186dc08d3963ac8afaaff484a0
ergemp <ergemp@gmail.com>
2020-06-03 18:37:43 +0300

    pearson correlation coefficient calculator added

--- src/main/java/org/ergemp/mlt/linearRegression/actor/Calculator.java
+++ src/main/java/org/ergemp/mlt/linearRegression/actor/Calculator.java
@@ -1,15 +1,17 @@
 package org.ergemp.mlt.linearRegression.actor;
 
+import org.ergemp.mlt.linearRegression.model.RegressionModel;
+
 import java.util.List;
 
 public class Calculator {
-    public static void calculate(List<Double> gArr1, List<Double> gArr2, Boolean debug){
-
-        Double meanArr1 = org.ergemp.mlt.pearsonsCorrelation.actor.getMean.get(gArr1, false);
-        Double meanArr2 = org.ergemp.mlt.pearsonsCorrelation.actor.getMean.get(gArr2, false);
+    public static RegressionModel calculate(List<Double> gArr1, List<Double> gArr2, Boolean debug){
 
         Double S1 = 0.0;
         Double S2 = 0.0;
+
+        Double meanArr1 = org.ergemp.mlt.pearsonsCorrelation.actor.getMean.get(gArr1, false);
+        Double meanArr2 = org.ergemp.mlt.pearsonsCorrelation.actor.getMean.get(gArr2, false);
 
         Double pearsonsCoeff =  org.ergemp.mlt.pearsonsCorrelation.actor.Calculator.calculate(gArr1, gArr2, false);
 
@@ -17,31 +19,27 @@ public class Calculator {
             S1 += Math.pow(l1-meanArr1,2) ;
         }
 
-        if (debug){
-            System.out.println("S1: " + S1);
-        }
-
         S1 = Math.sqrt(S1/(gArr1.size()-1));
-
-        if (debug){
-            System.out.println("S1: " + S1);
-        }
 
         for (Double l2 : gArr2) {
             S2 += Math.pow(l2-meanArr2,2) ;
         }
 
-        if (debug){
-            System.out.println("S2: " + S2);
-        }
-
         S2 = Math.sqrt(S2/(gArr2.size()-1));
 
+        Double b = pearsonsCoeff * (S2/S1);
+        Double a = meanArr2 - (b*meanArr1);
+
         if (debug){
+            System.out.println("S1: " + S1);
             System.out.println("S2: " + S2);
+            System.out.println("pearsonsCoeff: " + pearsonsCoeff);
+
+            System.out.println("b: " + b);
+            System.out.println("a: " + a);
         }
 
-
+        return (new RegressionModel(a,b));
 
     }
 }
--- src/main/java/org/ergemp/mlt/linearRegression/examples/example01.java
+++ src/main/java/org/ergemp/mlt/linearRegression/examples/example01.java
@@ -1,6 +1,7 @@
 package org.ergemp.mlt.linearRegression.examples;
 
 import org.ergemp.mlt.linearRegression.actor.Calculator;
+import org.ergemp.mlt.linearRegression.model.RegressionModel;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +12,9 @@ public class example01 {
         List<Double> arr1 = Arrays.asList(17.0,13.0,12.0,15.0,16.0,14.0,16.0,16.0,18.0,19.0);
         List<Double> arr2 = Arrays.asList(94.0,73.0,59.0,80.0,93.0,85.0,66.0,79.0,77.0,91.0);
 
-        Calculator.calculate(arr1,arr2,true);
+        RegressionModel model = Calculator.calculate(arr1,arr2,false);
+
+        System.out.println(model.getY(15.0));
 
     }
 }
